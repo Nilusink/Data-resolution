d1ccd490c589e648b1ec73b7acdc4b900ea1293d
Nilusink <65816512+Nilusink@users.noreply.github.com>
2021-02-27 19:25:00 +0100

    New File: dirSize.py, Tree Directory multi-threade

--- Tree directory.py
+++ Tree directory.py
@@ -1,67 +1,97 @@
 from os import system, listdir, path
+import concurrent.futures
 from tkinter import filedialog, Tk
+from time import time
+
+class MyClass:
+    def __init__(self):
+        self.dirs = list()
+        pass
+
+    def myfunc(self, direcotrys):
+        direcotrys = [direcotrys]
+        invalid = list()
+        newelements = int()
+        while True:
+            try:
+                for dire in direcotrys:
+                    if path.isdir(dire):
+                        new = listdir(dire)
+                        newelements+=len(new)
+                        for element in new:
+                            print(dire+element, end='\r')
+                            if not dire+'/'+element in direcotrys and not dire+'/'+element in invalid:
+                                if dire=='C:/':
+                                    if path.isdir(dire+'/'+element):
+                                        self.dirs.append(dire+'/'+element)
+
+                                else:
+                                    if path.isdir(dire+'/'+element):
+                                        self.dirs.append(dire+'/'+element)
+                                    else:
+                                        self.dirs.append(dire+'/'+element)
+                if newelements<=0:
+                    print(newelements)
+                    break
+
+                newelements = 0
+
+            except PermissionError:
+                direcotrys.remove(dire)
+                print('Peremission Denied: '+dire)
+                invalid.append(dire)
+            
+            except Exception as e:
+                print(e)
+
+        return self.dirs
 
 
-paths = list()
-files = list()
+if __name__=='__main__':
+    paths = list()
+    files = list()
 
-root = Tk()
-dir = filedialog.askdirectory()+'/'
-root.destroy()
+    root = Tk()
+    dir = filedialog.askdirectory()+'/'
+    root.destroy()
 
-paths2 = [dir+x for x in listdir(dir)]
+    paths2 = [dir+x for x in listdir(dir)]
 
-invalid = list()
 
-scanned_pahts = list()
-def is_scanned(path):
-    return path in scanned_paths
+    scanned_pahts = list()
+    def is_scanned(path):
+        return path in scanned_pahts
 
-newelements = int()
+    newelements = int()
 
-while True:
-    try:
-        for dire in paths2:
-            if path.isdir(dire):
-                new = listdir(dire)
-                for element in new:
-                    print(dire+element, end='\r')
-                    if not dire+'/'+element in paths2 and not dire+'/'+element in invalid:
-                        if dire=='C:/':
-                            if path.isdir(dire+'/'+element):
-                                paths2.append(dire+'/'+element)
+    clas = MyClass()
+    start = time()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for elements in executor.map(clas.myfunc, paths2):
+            for element in elements:
+                #print(elements)
+                if not element in paths2:
+                    paths2.append(element)
 
-                        else:
-                            if path.isdir(dire+'/'+element):
-                                paths2.append(dire+'/'+element)
-                            else:
-                                paths2.append(dire+'/'+element)
+    end = time()
 
-        if not newelements:
-            break
+    paths2.sort()
+    #print(paths2)
+    for element in paths2:
+        #print(element)
+        elements = element.split('/')
+        elLen = len(elements)
 
-        newelements = 0
+        if path.isdir(element):
+            for i in range(elLen-len(dir.split('/'))):
+                print('    |', end='')
 
-    except PermissionError:
-        paths2.remove(dire)
-        print('Peremission Denied: '+dire)
-        invalid.append(dire)
+            print('----'+elements[elLen-1])
 
-paths2.sort()
-
-for element in paths2:
-    elements = element.split('/')
-    elLen = len(elements)
-
-    if path.isdir(element):
-        for i in range(elLen-len(dir.split('/'))):
-            print('    |', end='')
-
-        print('----'+elements[elLen-1])
-
-    else:
-        for i in range(elLen-len(dir.split('/'))):
-            print('    |', end='')
-        print('----'+elements[elLen-1])
-
-input('\n\nPress Enter to exit')
+        else:
+            for i in range(elLen-len(dir.split('/'))):
+                print('    |', end='')
+            print('----'+elements[elLen-1])
+    
+    print('\n\n\n',end-start)
+    input('\n\nPress Enter to exit')
---
+++ dirSize.py
@@ -0,0 +1,43 @@
+from os import walk, listdir
+from os.path import join, getsize, isdir
+from tkinter import filedialog, Tk
+
+def GetFolderSize(path):
+    TotalSize = 0
+    for item in walk(path):
+        for file in item[2]:
+            try:
+                TotalSize = TotalSize + getsize(join(item[0], file))
+            except:
+                print("error with file:  " + join(item[0], file))
+    return TotalSize
+
+root = Tk()
+direc = filedialog.askdirectory()+'/'
+root.destroy()
+dirs = list()
+for element in listdir(direc):
+    if isdir(direc+element):
+        dirs.append([direc+element, float(GetFolderSize(direc+element)) /1024 /1024 /1024])
+
+max = [str(), int()]
+min = [str(), int()]
+for element in dirs:
+    print(f'{element[0]}: {round(element[1], 2)} GB')
+
+    if element[1]==max[1]:
+        max = [f'{max[0]} \n{element}', element[1]]
+    elif element[1]>max[1]:
+        max = [element[0], element[1]]
+    
+    if element[1]==min[1]:
+        min = [f'{min[0]} \n{element[0]}', element[1]]
+    elif element[1]<min[1]:
+        min = [element[0], element[1]]
+    
+
+print(f'\nBiggest folder(s): {max[0]} with {round(max[1], 2)} GB stored')
+print(f'\nSmallest folder(S): {min[0]} with {round(min[1], 2)} GB stored')
+
+
+input()
